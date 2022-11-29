122839c4dec216891db2f8654c0e447e472b1b2c
SirJohn42Walker <111967604+SirJohn42Walker@users.noreply.github.com>
2022-11-29 12:13:41 +0100

    move the navbar from index.js to _app.js for global rendering

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,7 @@
-import Navbar from "../components/Navbar/Navbar";
-
 export default function Home() {
   return (
     <>
       <h1>Willkommen zum 🐙-Capstone-Template!</h1>
-      <Navbar />
     </>
   );
 }
