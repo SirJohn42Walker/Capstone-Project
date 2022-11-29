3baca963a0693ad022d2ad7f053e1229d05ce25a
SirJohn42Walker <111967604+SirJohn42Walker@users.noreply.github.com>
2022-11-29 11:24:23 +0100

    add title tags to icon svgs

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,10 @@
+import Navbar from "../components/Navbar/Navbar";
+
 export default function Home() {
-  return <h1>Willkommen zum 🐙-Capstone-Template!</h1>;
+  return (
+    <>
+      <h1>Willkommen zum 🐙-Capstone-Template!</h1>
+      <Navbar />
+    </>
+  );
 }
