9e741502506a1764247837305963e0513aaebb17
SirJohn42Walker <111967604+SirJohn42Walker@users.noreply.github.com>
2022-12-02 11:42:34 +0100

    add profile page filled with placeholders

--- pages/profile.js
+++ pages/profile.js
@@ -1,4 +1,3 @@
-import styled from "styled-components";
 import Textbox from "../components/Boxes/Textbox";
 import BasicBG from "../components/Backgrounds/Basic-BG";
 
@@ -6,6 +5,12 @@ export default function Profile() {
   return (
     <>
       <BasicBG />
+      <Textbox>
+        Username: <br />
+        Max Musterghoti
+      </Textbox>
+      <Textbox>Level Reached: 6</Textbox>
+      <Textbox>Time spent ingame: 66 Minutes</Textbox>
     </>
   );
 }
