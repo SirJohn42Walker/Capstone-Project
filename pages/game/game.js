73bfd4cc72f8cd991690ae25a8dd951a856540d3
SirJohn42Walker <111967604+SirJohn42Walker@users.noreply.github.com>
2022-12-05 17:09:29 +0100

    add data array for text in lvl1

--- pages/game/game.js
+++ pages/game/game.js
@@ -1,10 +1,18 @@
 import Textbox from "../../components/Boxes/Textbox";
 import StyledMain from "../../components/Backgrounds/Basic-BG";
+import {useState} from "react";
+import {level1Data} from "../../components/LevelData/Level1Data";
 
 export default function Game() {
+  const [textState, setTextState] = useState(0);
+  function furtherButton() {
+    setTextState(textState + 1);
+  }
+
   return (
     <StyledMain>
-      <Textbox>Placeholder.</Textbox>
+      <Textbox>{level1Data.text[textState]}</Textbox>
+      <button onClick={furtherButton}>Go on...</button>
     </StyledMain>
   );
 }
