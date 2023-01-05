b8f3c23cf689c67d60536db0a3e0a48c24ff8b43
SirJohn42Walker <111967604+SirJohn42Walker@users.noreply.github.com>
2023-01-05 16:48:54 +0100

    some more stuff

--- components/Boxes/Interactive Boxes/AnswerForm.js
+++ components/Boxes/Interactive Boxes/AnswerForm.js
@@ -34,7 +34,7 @@ export function AnswerForm({
       ></InputField>
       {answerMessage && <DialogBox>{answerMessage}</DialogBox>}
       <ForwardButton type="submit" disabled={submitting}>
-        Obviously this.
+        Of course this is the answer.
       </ForwardButton>
     </form>
   );
@@ -44,7 +44,7 @@ const InputField = styled.input`
   background-color: var(--color13);
   color: var(--color2);
   position: fixed;
-  bottom: 30vh;
+  bottom: 28vh;
   left: 50%;
   border-radius: 15px;
   padding: 5px;
